34a7f1f5d5c1d8b84a3c38c60268c30a2d581acf
Leo Donati <leo.donati@unice.fr>
2022-10-14 10:33:18 +0200

    page 54 du book GoodParts

--- scripts/program.js
+++ scripts/program.js
@@ -2,16 +2,31 @@ document.writeln("Hello world !");
 
 if (typeof Object.create !== 'function') {
     Object.create = function (o) {
-        var F = function () {};
+        var F = function () { };
         F.prototype = o;
         return new F();
     };
 }
 
-var add = function(x,y) {
+var add = function (x, y) {
     return x + y;
 }
 
 var res = add(3, 4);
 
 console.log(res);
+
+// Define a function that sets a DOM node's color
+// to yellow and then fades it to white.
+var fade = function (node) {
+    var level = 1;
+    var step = function () {
+        var hex = level.toString(16); node.style.backgroundColor = '#FFFF' + hex + hex; if (level < 15) {
+            level += 1;
+            setTimeout(step, 100);
+        }
+    };
+    setTimeout(step, 100);
+};
+
+fade(document.body);
